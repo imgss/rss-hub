c259405b5b7694d09ba76655e515d4b31bca827b
imgss <838632526@qq.com>
2019-01-09 14:55:48 +0800

    加入订阅按钮

--- lib/tabs/allrss.dart
+++ lib/tabs/allrss.dart
@@ -15,8 +15,10 @@ class _AllRssState extends State<AllRss>
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this);
+    print('start');
     http.get('https://rsshub.app/api/routes').then((res){
       Map _res = json.decode(res.body);
+      print(res.body);
       if (res.statusCode == 200) {
         setState(() {
           routes = _res['data'];
@@ -25,6 +27,8 @@ class _AllRssState extends State<AllRss>
       } else {
         print('error');
       }
+    }).catchError((){
+      print('error');
     });
   }
 
@@ -51,11 +55,45 @@ class _AllRssState extends State<AllRss>
           return ListTile(
             onTap: () {
               print(routes[routeNames[index]]);
+              List subRoutes = routes[routeNames[index]]['routes'];
+              showBottomSheet(
+                context: context,
+                builder: (context){
+                  return ListView.separated(
+                    padding: EdgeInsets.only(top: 20.0),
+                    itemCount: subRoutes.length,
+                    separatorBuilder: (context, index){
+                      return Divider(
+                        color: Colors.black12,
+                      );
+                    },
+                    itemBuilder: (context, index){
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              subRoutes[index],
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+
+                          FlatButton(
+                            textColor: Colors.blueAccent,
+                            onPressed: () {
+                              print(1);
+                            },
+                            child: Text('订阅')
+                          )
+                        ]);
+                    },
+                  );
+                }
+              );
             },
             title: Text(routeNames[index]),
             subtitle: Padding(
               padding: EdgeInsets.symmetric(vertical: 2.0),
-              child: Text('qwer'),
+              child: Text('有${routes[routeNames[index]]['routes'].length}个RSS订阅'),
             ),
           );
         },
